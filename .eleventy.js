c956c4f1f69e52651c16b82b17146da9365f4813
Sophie Koonin <sophiekoonin@monzo.com>
2022-09-28 14:56:05 +0100

    fix everything

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 // Filters
-const debugFilter = require('./src/filters/debug');
-const markdownIt = require('markdown-it');
-const markdownItAttrs = require('markdown-it-attrs');
+const debugFilter = require("./src/filters/debug");
+const webmentionsFilter = require("./src/filters/webmentions");
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
 
 const markdownItOptions = {
   html: true,
@@ -11,21 +12,28 @@ const markdownItOptions = {
 const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs);
 
 module.exports = (config) => {
-  config.addWatchTarget('./src');
+  config.addWatchTarget("./src");
 
-  config.addPassthroughCopy({ './src/static': '/' });
-  config.addPassthroughCopy({ './src/js': '/' });
-  config.setLibrary('md', markdownLib);
+  config.addPassthroughCopy({ "./src/static": "/" });
+  config.addPassthroughCopy({ "./src/js": "/" });
+  config.setLibrary("md", markdownLib);
+  config.addFilter("head", (array, n) => {
+    if (n < 0) {
+      return array.slice(n);
+    }
 
-  config.addFilter('debug', debugFilter);
+    return array.slice(0, n);
+  });
+  config.addFilter("webmentionsForUrl", webmentionsFilter);
+  config.addFilter("debug", debugFilter);
 
   return {
     dir: {
-      input: 'src',
-      output: '_site',
+      input: "src",
+      output: "_site",
     },
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
   };
 };
